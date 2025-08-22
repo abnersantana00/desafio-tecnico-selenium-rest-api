523cd99f16acae7f50af8a4ceae24fa90ad55cc7
Abner de Santana Silva <100228850+abnersantana00@users.noreply.github.com>
2025-08-22 17:02:48 -0300

    remove link publico

--- app/models.py
+++ app/models.py
@@ -7,7 +7,6 @@ class DomPublicacao(Base):
 
     id = Column(BigInteger, primary_key=True, autoincrement=True)
     url_publica = Column(Text, nullable=False)   # link do 0x0.st/001.pdf
-    url_local = Column(Text, nullable=False)     # ex.: data/2025-07/DOM_7123.pdf
     competencia = Column(String(7), nullable=False) # VARCHAR(7) NOT NULL no formato 'MM/AA'
     # BOOLEAN NOT NULL DEFAULT FALSE
     is_extra = Column(Boolean, nullable=False, server_default=text("false"))
